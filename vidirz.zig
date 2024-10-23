22c33bd84de53486e00ba7653f69dfd362c309ba
Ludvig Gunne Lindström <ludviggunnelindstrom@gmail.com>
2024-10-24 01:48:29 +0200

    delete temporary

--- vidirz.zig
+++ vidirz.zig
@@ -144,180 +144,188 @@ pub fn main() !void {
             try temp.writer().print("{d:0>4}    {s}\n", .{ i, entry.name });
         }
     }
-    defer std.fs.cwd().deleteFile(temp_name) catch {};
-
-    // Determine editor command
-    const default_editor_dup = try allocator.dupe(u8, default_editor);
-    var use_default_editor = false;
-    const editor = get_editor: {
-        var env = std.process.getEnvMap(allocator) catch |e| {
-            try stderr.print(
-                warning_str ++ "unable to read environment map: {s}. Using default editor '{s}'.\n",
-                .{ @errorName(e), default_editor_dup },
-            );
-            use_default_editor = true;
-            break :get_editor default_editor_dup;
-        };
-        defer env.deinit();
-
-        break :get_editor try allocator.dupe(u8, env.get("EDITOR") orelse {
-            try stderr.print(
-                warning_str ++ "EDITOR not set, using default '{s}'\n",
-                .{default_editor_dup},
-            );
-            use_default_editor = true;
-            break :get_editor default_editor_dup;
-        });
-    };
-    if (!use_default_editor) {
-        allocator.free(default_editor_dup);
-    }
-    defer allocator.free(editor);
-
-    // Run editor command
-    var child = std.process.Child.init(&.{ editor, temp_name }, allocator);
-    const term = child.spawnAndWait() catch |e| die(
-        err_str ++ "unable to run '{s}': {s}\n",
-        .{ editor, @errorName(e) },
-    );
-
-    switch (term) {
-        .Exited => |code| if (code != 0)
-            die(err_str ++ "{s} returned {d}\n", .{ editor, code }),
-        else => die(err_str ++ "{s} did not exit\n", .{editor}),
-    }
-
-    // Read modified file
-    {
-        var temp = std.fs.cwd().openFile(temp_name, .{}) catch |e| die(
-            err_str ++ "unable to reopen temporary: {s}\n",
-            .{@errorName(e)},
-        );
-        defer temp.close();
-
-        const reader = temp.reader();
-
-        var buf = std.ArrayList(u8).init(allocator);
-        defer buf.deinit();
-
-        const num_orig_entries = entries.items.len;
-        read_loop: while (true) {
-            buf.clearRetainingCapacity();
-
-            reader.streamUntilDelimiter(buf.writer(), ' ', null) catch |e| {
-                switch (e) {
-                    error.EndOfStream => break :read_loop,
-                    else => die(
-                        err_str ++ "while reading from temporary: {s}\n",
-                        .{@errorName(e)},
-                    ),
-                }
-            };
-
-            const index = std.fmt.parseInt(usize, buf.items, 10) catch |e| die(
-                err_str ++ "parse error at '{s}': {s}\n",
-                .{ buf.items, @errorName(e) },
-            );
-
-            if (index > num_orig_entries)
-                die(
-                    err_str ++ "too big index in temporary ({d} > {d})\n",
-                    .{ index, num_orig_entries },
-                );
-
-            if (entries.items[index].action != .delete)
-                die(err_str ++ "duplicate index {d}\n", .{index});
-
-            // Read filename
-            buf.clearRetainingCapacity();
-
-            reader.streamUntilDelimiter(buf.writer(), '\n', null) catch |e| die(
-                err_str ++ "while reading filename for index {d}: {s}\n",
-                .{ index, @errorName(e) },
-            );
-
-            const filename = try allocator.dupe(u8, std.mem.trim(u8, buf.items, " "));
-
-            var entry = &entries.items[index];
-
-            // Update corresponding entry
-            if (std.mem.eql(u8, entry.name, filename)) {
-                allocator.free(filename);
-                entry.action = .keep;
-            } else {
-                entry.action = .{ .rename = filename };
-            }
-        }
-    }
 
     var exit_code: u8 = 0;
+    {
+        defer {
+            std.fs.cwd().deleteFile(temp_name) catch {
+                stderr.print(warning_str ++ "couldn't delete temporary {s}\n", .{temp_name}) catch {};
+            };
+        }
 
-    // Execute
-    for (entries.items) |entry| {
-        const stat = std.fs.cwd().statFile(entry.name) catch |e| {
-            try stderr.print(err_str ++ "unable to stat file {s}: {s}\n", .{ entry.name, @errorName(e) });
-            exit_code = 1;
-            continue;
-        };
-
-        const is_dir = switch (stat.kind) {
-            .directory => true,
-            .file => false,
-            else => {
-                try stdout.print(
-                    "Skipping {s} {s}\n",
-                    .{ @tagName(stat.kind), entry.name },
+        // Determine editor command
+        const default_editor_dup = try allocator.dupe(u8, default_editor);
+        var use_default_editor = false;
+        const editor = get_editor: {
+            var env = std.process.getEnvMap(allocator) catch |e| {
+                try stderr.print(
+                    warning_str ++ "unable to read environment map: {s}. Using default editor '{s}'.\n",
+                    .{ @errorName(e), default_editor_dup },
                 );
-                continue;
-            },
+                use_default_editor = true;
+                break :get_editor default_editor_dup;
+            };
+            defer env.deinit();
+
+            break :get_editor try allocator.dupe(u8, env.get("EDITOR") orelse {
+                try stderr.print(
+                    warning_str ++ "EDITOR not set, using default '{s}'\n",
+                    .{default_editor_dup},
+                );
+                use_default_editor = true;
+                break :get_editor default_editor_dup;
+            });
         };
+        if (!use_default_editor) {
+            allocator.free(default_editor_dup);
+        }
+        defer allocator.free(editor);
 
-        switch (entry.action) {
-            .keep => if (verbose) try stdout.print("keep   {s}\n", .{entry.name}),
+        // Run editor command
+        var child = std.process.Child.init(&.{ editor, temp_name }, allocator);
+        const term = child.spawnAndWait() catch |e| die(
+            err_str ++ "unable to run '{s}': {s}\n",
+            .{ editor, @errorName(e) },
+        );
 
-            .rename => |new_name| {
-                if (!interactive or try prompt(
-                    "Rename '{s}' to '{s}'?",
-                    .{ entry.name, new_name },
-                )) {
-                    std.fs.cwd().rename(entry.name, new_name) catch |e| {
-                        try stderr.print(
-                            err_str ++ "unable to rename {s}: {s}\n",
-                            .{ entry.name, @errorName(e) },
-                        );
-                        exit_code = 1;
-                        continue;
-                    };
-                    if (verbose) try stdout.print("rename {s} to {s}.\n", .{ entry.name, new_name });
-                }
-            },
+        switch (term) {
+            .Exited => |code| if (code != 0)
+                die(err_str ++ "{s} returned {d}\n", .{ editor, code }),
+            else => die(err_str ++ "{s} did not exit\n", .{editor}),
+        }
 
-            .delete => {
-                if (!interactive or try prompt("Delete {s}?", .{entry.name})) {
-                    if (is_dir) {
-                        if (!force and !try prompt("Delete directory {s}?", .{entry.name}))
-                            continue;
-                        std.fs.cwd().deleteDir(entry.name) catch |e| {
-                            try stderr.print(
-                                err_str ++ "unable to delete {s}: {s}\n",
-                                .{ entry.name, @errorName(e) },
-                            );
-                            exit_code = 1;
-                            continue;
-                        };
-                    } else {
-                        std.fs.cwd().deleteFile(entry.name) catch |e| {
-                            try stderr.print(
-                                err_str ++ "unable to delete {s}: {s}\n",
-                                .{ entry.name, @errorName(e) },
-                            );
-                            exit_code = 1;
-                            continue;
-                        };
+        // Read modified file
+        {
+            var temp = std.fs.cwd().openFile(temp_name, .{}) catch |e| die(
+                err_str ++ "unable to reopen temporary: {s}\n",
+                .{@errorName(e)},
+            );
+            defer temp.close();
+
+            const reader = temp.reader();
+
+            var buf = std.ArrayList(u8).init(allocator);
+            defer buf.deinit();
+
+            const num_orig_entries = entries.items.len;
+            read_loop: while (true) {
+                buf.clearRetainingCapacity();
+
+                reader.streamUntilDelimiter(buf.writer(), ' ', null) catch |e| {
+                    switch (e) {
+                        error.EndOfStream => break :read_loop,
+                        else => die(
+                            err_str ++ "while reading from temporary: {s}\n",
+                            .{@errorName(e)},
+                        ),
                     }
-                    if (verbose) try stdout.print("delete {s}\n", .{entry.name});
+                };
+
+                const index = std.fmt.parseInt(usize, buf.items, 10) catch |e| die(
+                    err_str ++ "parse error at '{s}': {s}\n",
+                    .{ buf.items, @errorName(e) },
+                );
+
+                if (index > num_orig_entries)
+                    die(
+                        err_str ++ "too big index in temporary ({d} > {d})\n",
+                        .{ index, num_orig_entries },
+                    );
+
+                if (entries.items[index].action != .delete)
+                    die(err_str ++ "duplicate index {d}\n", .{index});
+
+                // Read filename
+                buf.clearRetainingCapacity();
+
+                reader.streamUntilDelimiter(buf.writer(), '\n', null) catch |e| die(
+                    err_str ++ "while reading filename for index {d}: {s}\n",
+                    .{ index, @errorName(e) },
+                );
+
+                const filename = try allocator.dupe(u8, std.mem.trim(u8, buf.items, " "));
+
+                var entry = &entries.items[index];
+
+                // Update corresponding entry
+                if (std.mem.eql(u8, entry.name, filename)) {
+                    allocator.free(filename);
+                    entry.action = .keep;
+                } else {
+                    entry.action = .{ .rename = filename };
                 }
-            },
+            }
+        }
+
+        // Execute
+        for (entries.items) |entry| {
+            const stat = std.fs.cwd().statFile(entry.name) catch |e| {
+                try stderr.print(err_str ++ "unable to stat file {s}: {s}\n", .{ entry.name, @errorName(e) });
+                exit_code = 1;
+                continue;
+            };
+
+            const is_dir = switch (stat.kind) {
+                .directory => true,
+                .file, .named_pipe => false,
+                else => {
+                    if (entry.action != .keep) {
+                        try stdout.print(
+                            "Skipping {s} {s}\n",
+                            .{ @tagName(stat.kind), entry.name },
+                        );
+                    }
+                    continue;
+                },
+            };
+
+            switch (entry.action) {
+                .keep => if (verbose) try stdout.print("keep   {s}\n", .{entry.name}),
+
+                .rename => |new_name| {
+                    if (!interactive or try prompt(
+                        "Rename '{s}' to '{s}'?",
+                        .{ entry.name, new_name },
+                    )) {
+                        std.fs.cwd().rename(entry.name, new_name) catch |e| {
+                            try stderr.print(
+                                err_str ++ "unable to rename {s}: {s}\n",
+                                .{ entry.name, @errorName(e) },
+                            );
+                            exit_code = 1;
+                            continue;
+                        };
+                        if (verbose) try stdout.print("rename {s} to {s}.\n", .{ entry.name, new_name });
+                    }
+                },
+
+                .delete => {
+                    if (!interactive or try prompt("Delete {s}?", .{entry.name})) {
+                        if (is_dir) {
+                            if (!force and !try prompt("Delete directory {s}?", .{entry.name}))
+                                continue;
+                            std.fs.cwd().deleteDir(entry.name) catch |e| {
+                                try stderr.print(
+                                    err_str ++ "unable to delete {s}: {s}\n",
+                                    .{ entry.name, @errorName(e) },
+                                );
+                                exit_code = 1;
+                                continue;
+                            };
+                        } else {
+                            std.fs.cwd().deleteFile(entry.name) catch |e| {
+                                try stderr.print(
+                                    err_str ++ "unable to delete {s}: {s}\n",
+                                    .{ entry.name, @errorName(e) },
+                                );
+                                exit_code = 1;
+                                continue;
+                            };
+                        }
+                        if (verbose) try stdout.print("delete {s}\n", .{entry.name});
+                    }
+                },
+            }
         }
     }
 
